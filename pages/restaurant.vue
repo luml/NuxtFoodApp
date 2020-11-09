a659e6b5efebeeff8e165740970ae38b700bf298
luml <lumeiling@bonc.com.cn>
2020-11-09 15:24:04 +0800

    feat: filter component

--- pages/restaurant.vue
+++ pages/restaurant.vue
@@ -1,16 +1,43 @@
 <template>
   <main class="container restaurrant">
-    <h1>Restaurants</h1>
-    <AppRestaurantInfo />
+    <div class="restaurantheading">
+      <h1>Restaurants</h1>
+      <AppSelect @change="selectedResaurant = $event" />
+      <pre>{{ selectedResaurant }}</pre>
+    </div>
+    <AppRestaurantInfo :datasource="filteredRestaurants"/>
   </main>
 </template>
 
 <script>
 import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue';
+import AppSelect from '@/components/AppSelect.vue';
+import { mapState } from 'vuex';
+
 export default {
   components: {
-    AppRestaurantInfo
+    AppRestaurantInfo,
+    AppSelect
   },
+  data() {
+    return {
+      selectedResaurant: '',
+    }
+  },
+  computed: {
+    ...mapState([
+      'fooddata',
+    ]),
+    filteredRestaurants() {
+      if (this.selectedResaurant) {
+        return this.fooddata.filter(el => {
+          let name = el.name.toLowerCase()
+          return name.includes(this.selectedResaurant)
+        })
+      }
+      return this.fooddata
+    }
+  }
 }
 </script>
 
